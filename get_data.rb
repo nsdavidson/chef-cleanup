88b090e4bdd97235b6e2b11c0111b2933f1f85d4
Nolan Davidson <ndavidson@chef.io>
2017-05-30 20:34:47 -0400

    Adding --all-orgs option
    Added an option to fetch all orgs from the Chef server to process.
    
    Signed-off-by: Nolan Davidson <ndavidson@chef.io>

--- get_data.rb
+++ get_data.rb
@@ -1,6 +1,7 @@
 require 'chef'
 require 'json'
 require 'optparse'
+require 'uri'
 
 def output_list(list)
   list.each do |name, versions|
@@ -44,28 +45,45 @@ def get_unused_cookbooks(used_list, cb_list)
   unused_list
 end
 
+def get_all_orgs(server_root)
+  chef_endpoint = Chef::ServerAPI.new("#{server_root}")
+  orgs = chef_endpoint.get_rest('organizations')
+  org_list = []
+  orgs.each do |name, url|
+    org_list << name
+  end
+  org_list
+end
+
 options = {}
 OptionParser.new do |opt|
   opt.on('--orgs ORG1,ORG2') { |o| options[:org_list] = o }
+  opt.on('--all-orgs') { |o| options[:all_orgs] = o }
   opt.on('--node-threshold NUM_DAYS') { |o| options[:threshold_days] = o }
   opt.on('--knife-config PATH_TO_KNIFE_RB') { |o| options[:knife_config] = o }
 end.parse!
 
-orgs = options[:org_list].split(',') || ['myorg']
-threshold_in_days = options[:threshold_days] || 30
 knife_config = options[:knife_config] || "#{ENV['HOME']}/.chef/knife.rb"
 
 Chef::Config.from_file(knife_config)
+chef_server_root = URI.join(Chef::Config['chef_server_url'], "/").to_s
+orgs =  if options[:all_orgs]
+          get_all_orgs(chef_server_root)
+        elsif options[:org_list]
+          options[:org_list].split(',')
+        else
+          ['myorg']
+        end
+threshold_in_days = options[:threshold_days] || 30
 
-chef_server_root = Chef::Config['chef_server_url'][/.*\//]
 stale_orgs = []
 orgs.each do |org|
-  chef_endpoint = Chef::ServerAPI.new("#{chef_server_root}#{org}")
+  chef_endpoint = Chef::ServerAPI.new("#{chef_server_root}/organizations/#{org}")
   puts "Processing organization #{org}..."
   cb_list = get_cookbook_list(chef_endpoint)
   version_count = get_cookbook_count(cb_list).sort_by(&:last).reverse.to_h
   output_list(version_count)
-  nodes = Chef::Search::Query.new("#{chef_server_root}#{org}").search(:node, '*:*', :filter_result => {'name' => ['name'], 'cookbooks' => ['cookbooks'], 'ohai_time' => ['ohai_time']} )
+  nodes = Chef::Search::Query.new("#{chef_server_root}/organizations/#{org}").search(:node, '*:*', :filter_result => {'name' => ['name'], 'cookbooks' => ['cookbooks'], 'ohai_time' => ['ohai_time']} )
   used_cookbooks = {}
   #nodes[0].select{|node| node.class == Array}.each do |node|
   nodes[0].select{|node| !node['cookbooks'].nil?}.each do |node|
